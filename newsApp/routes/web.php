548a13dff3e023d666a63fcf0833299179c34e6e
unknown <banbe417@gmail.com>
2025-02-17 16:14:55 +0700

    Tugas 13 : Laravel templating

--- newsApp/routes/web.php
+++ newsApp/routes/web.php
@@ -17,3 +17,10 @@ use App\Http\Controllers\AuthController;
 Route::get('/', [DashboardController::class, 'home'])->name('home');
 Route::get('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/welcome', [AuthController::class, 'welcome'])->name('welcome');
+Route::get('/table', function () {
+    return view('table');
+})->name('table');
+
+Route::get('/data-tables', function () {
+    return view('data-tables');
+})->name('data-tables');
